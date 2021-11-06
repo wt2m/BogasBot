8c218548654262f0ca3cfef188365bc7ac2fe724
wt2m <52261424+wt2m@users.noreply.github.com>
2021-11-06 03:27:08 -0300

    Função de adicionar gifs a base

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import json
 import dotenv
 import os
+import random
 
 dotenv.load_dotenv(dotenv.find_dotenv());
 
@@ -18,16 +19,23 @@ async def on_message(message):
     if message.author == bot.user:
         return
     '''if message.author.id == 204350761616932865:
-        await message.reply("https://tenor.com/view/femboy-skirt-maid-outift-maid-outfit-gif-21382375")
+        await message.reply("https://tenor.com/view/femboy-skirt-maid-outift-maid-outfit-gif-21382375")'''
         
     if message.author.id == 359163391375441920:
-        await message.reply("Vai se fude gamer fedorento")
-        await message.reply("https://tenor.com/view/aaaaa-aaaaaaa-shout-yell-rat-gif-14860400")  '''
-    if "teste" in message.content:
-        await message.channel.send(
-            f"Por favor, {message.author.name}, quica na minha pica! {message}"
+        r = random.randint(0,50)
+        if r == 21:
+            await message.reply("Vai se fude gamer fedorento")
+        if r == 40:
+            await message.reply("https://tenor.com/view/aaaaa-aaaaaaa-shout-yell-rat-gif-14860400")
+    if "Hiro" in message.content:
+        await message.reply(
+            f"Por favor, {message.author.name}, quica na minha pica e nunca mais cite esse random odiador de pobres!"
         )
-        await message.delete()
+    r = random.randint(0,400)
+    if r == 321:
+        await message.channel.send(
+            "aaaaaaaaaaaaaaaa cala a bocaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        )
     await bot.process_commands(message)
 
 @bot.command(name="msg")
@@ -40,32 +48,26 @@ async def send_msg(ctx):
 
 @bot.command(name="gifs")
 async def send_gifs(ctx):
-    gifs = [
-        'https://tenor.com/view/lithiumare-kiracord-nerd-marbellu-gif-22666819',
-        'https://media.discordapp.net/attachments/786466436448780308/832346839365713981/a_e33526630f9c39bb2a1203bf01fe564a.gif',
-        'https://tenor.com/view/baguncinha-we-do-a-little-guei-gif-21158523',
-        'https://tenor.com/view/cum-the-cum-here-comes-the-cum-gif-18740280',
-        'https://tenor.com/view/kioo-gif-19634683',
-        'https://tenor.com/view/the-j-spongebob-gif-21742603',
-        'https://tenor.com/view/femboy-dance-dance-gif-cute-meme-gif-20533959',
-        'https://tenor.com/view/poop-massive-sump-naked-animation-ew-gif-17920176',
-        'https://tenor.com/view/squid-game-gi-hun-gi-hun-squid-game-seong-gi-hun-seung-gi-hun-gif-23386383',
-        'https://tenor.com/view/kitten-spoon-milk-drinking-milk-drinking-gif-19025805',
-        'https://media.discordapp.net/attachments/427586051079536640/893778269940350996/image0-18.gif',
-        'https://tenor.com/view/mustache-guy-crazy-mostacho-guy-with-crazy-mustache-gif-22094888',
-        'https://tenor.com/view/eq4a-humor-gif-22020263',
-        'https://tenor.com/view/momasoz-diego-2blocked-2blocked-messages-kaan%C3%B6zgal-%C3%B6mer-kaan%C3%B6zgal-gif-21824400',
-        'https://tenor.com/view/giant-cockroach-escaping-speedrun-bedrun-cockroach-ucd-gif-21935230',
-        'https://tenor.com/view/femboy-skirt-maid-outift-maid-outfit-gif-21382375',
-        'https://tenor.com/view/saudades-meu-amor-heart-love-couple-in-love-gif-16336939',
-        'https://tenor.com/view/aaaaa-aaaaaaa-shout-yell-rat-gif-14860400',
-    ]
+    fileObj = open('utils\gifs.txt', "r") #opens the file in read mode
+    gifs = fileObj.read().splitlines() #puts the file into an array
+    fileObj.close()
+    
     for gif in gifs:
         await ctx.send(gif)
 
+@bot.command(name="addgif")
+async def add_gif(ctx, *, arg):
+    fileObj = open("utils\gifs.txt")
+    old = fileObj.read();
+    print(old);
+    fileObj = open("utils\gifs.txt", "w")
+    fileObj.write(old)
+    fileObj.write(f"{arg}\n")
+    fileObj.close()
+    await ctx.message.reply('Armazenado meu truta')
+
 with open('utils\inconvenientes.json') as f:
     inconvenientes = json.load(f)
-    print(inconvenientes)
 
 @bot.command(name="inconveniente")
 async def inconveniente(ctx, *, arg):
